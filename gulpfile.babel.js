175222e3b751d500d34790460a945af9802f6148
Yesung <after.kubrick@gmail.com>
2021-08-15 17:39:59 +0900

    🔖 Deploy gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const routes = {
   pug: {
@@ -34,7 +35,7 @@ const routes = {
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build/"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
@@ -63,6 +64,8 @@ const js = () =>
     )
     .pipe(gulp.dest(routes.js.dest));
 
+const ghDeploy = () => gulp.src("build/**/*").pipe(ghPages());
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
@@ -74,6 +77,8 @@ const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug, styles, js]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([prepare, assets, live]);
+export const deploy = gulp.series([build, ghDeploy, clean]);
